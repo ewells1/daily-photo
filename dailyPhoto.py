e8fd24991db82ca68788b1186cb3af0eff272998
ewells1 <ewells1@binghamton.edu>
2015-01-04 19:17:01 -0500

    Added DailyPhoto.py

---
+++ dailyPhoto.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+
+captions = []
+
+def main():
+    currentDay = requests.get('http://binghamton.edu/photos/')
+    soup = bs4.BeautifulSoup(currentDay.text)
+    caption = soup.find('p').contents[0]
+    print (caption) # debug
+
+    while caption not in captions:
+        captions.append(caption)
+        nextLink = soup.select('a[href^="http://www.binghamton.edu/photos/index.php/gallery/archives/"]')[0].attrs.get('href')
+        print (nextLink) # debug
+        currentDay = requests.get(nextLink)
+        soup = bs4.BeautifulSoup(currentDay.text)
+        caption = soup.find('p').contents[0]
+        print (caption) # debug
+
+main()
