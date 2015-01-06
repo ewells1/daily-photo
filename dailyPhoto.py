87840be650cf6ba6e36d38b348c632d9b29e9ad4
ewells1 <ewells1@binghamton.edu>
2015-01-06 16:35:08 -0500

    Update dailyPhoto.py
    Instead of using captions to avoid an infinite loop, it now uses the links. This way it doesn't get tripped up on repeated captions and stop early.

--- dailyPhoto.py
+++ dailyPhoto.py
@@ -5,18 +5,21 @@ import bs4
 import csv
 
 captions = []
+links = []
 
 def main():
-    currentDay = requests.get('http://binghamton.edu/photos/')
+    nextLink = 'http://binghamton.edu/photos/'
+    currentDay = requests.get(nextLink)
     soup = bs4.BeautifulSoup(currentDay.text)
     #caption = soup.select(".caption")[1].p
-    caption = soup.find('p').contents[0]
-    print (caption) # debug
+    #caption = soup.find('p').contents[0]
+    #print (caption) # debug
     csvfile = open('captions.csv', 'wb')
     writer = csv.writer(csvfile)
 
-    while caption not in captions:
+    while nextLink not in links:
         captions.append(caption)
+        links.append(nextLink)
         nextLink = soup.select('a[href^="http://www.binghamton.edu/photos/index.php/gallery/archives/"]')[0].attrs.get('href')
         print (nextLink) # debug
         currentDay = requests.get(nextLink)
